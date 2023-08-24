5e7456f9d09d4f8bb836ea354a8e9df392269272
Keval0506 <114097127+Keval0506@users.noreply.github.com>
2023-08-24 23:25:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical4_21012011120/RegistrationActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mad_practical4_21012011120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+
+
+
+    }
+}
